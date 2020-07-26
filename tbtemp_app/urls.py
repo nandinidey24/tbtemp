77ff05338cf9afd2e7e17e9aa4edd38c15a88f0f
Nandini Dey <nandinisdey@gmail.com>
2020-07-26 20:49:33 +0530

    Media and static files done

--- tbtemp_app/urls.py
+++ tbtemp_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -11,3 +12,8 @@ urlpatterns = [
     path('subject', views.subject, name="subject"),
     path('document', views.document, name="document")
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
